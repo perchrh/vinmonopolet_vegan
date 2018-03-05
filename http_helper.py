a3e7e9135cf0edcdfaca68f649c1c0dbfc2f5a63
Per Christian Henden <perchrh-gh@pvv.org>
2018-03-05 18:25:24 +0100

    Update TODO

--- http_helper.py
+++ http_helper.py
@@ -26,7 +26,7 @@ def get_webpage(url):
 
     # use a fake custom user agent string to avoid silly webpages rejecting the library's default agent string
     custom_user_agent = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36"}
-    r = requests.get(url, headers=custom_user_agent, verify=False, timeout=5)
+    r = requests.get(url, headers=custom_user_agent, verify=False, timeout=30)
     r.raise_for_status()
     return r.text
 
